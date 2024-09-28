8078562734e08e09b807ed992cd9cb37a3a77eb4
JooCrepaldi <joaopedrocrepaldi05@gmail.com>
2024-09-28 20:33:14 -0300

    Personal Portfolio

---
+++ 03 - Personal Portfolio/darkmode.js
@@ -0,0 +1,25 @@
+let darkmode = localStorage.getItem("darkmode")
+const trocatema = document.getElementById("trocatema")
+
+const ligadodarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const desligadodarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', 'inactive')
+}
+
+if(darkmode === "active"){
+    ligadodarkmode()
+}
+
+trocatema.addEventListener("click", () => {
+    darkmode = localStorage.getItem("darkmode")
+    if(darkmode !== "active"){
+        ligadodarkmode()
+    } else {
+        desligadodarkmode()
+    }
+})
